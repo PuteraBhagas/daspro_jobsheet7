3da6e08f7c2cacd07a9c3fc94b8457eb5411d6e4
PuteraBhagas <p.bhagaswara@gmail.com>
2023-11-01 15:47:42 +0700

    percobaan while

--- WhileKelipatan25.java
+++ WhileKelipatan25.java
@@ -1,32 +1,25 @@
 import java.util.Scanner;
 
-/**
- * ForKelipatan25
- */
-public class ForKelipatan25 {
-
+public class WhileKelipatan25 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        int kelipatan, jumlah = 0, counter = 0;
-        double rata = 0;
         
+        int kelipatan, jumlah = 0, counter = 0;
 
         System.out.println("Masukan Bilangan Kelipatan (1-9): ");
         kelipatan = scan.nextInt();
 
-        for (int i = 1; i <= 50; i++) {
+        int i = 1;
+        while ( i <= 50 ) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
 
                 
-            }
-            rata = (double) jumlah / counter;
+            } i++;
         } 
                 System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
                 System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-                System.out.println("Rata - rata: " + rata);
 
     }
 }
